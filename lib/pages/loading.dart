4ed24b4d4b9c599ba1d4cb0c08d733afc8f855dc
Delwin Mathew <delu801@gmail.com>
2023-02-10 11:48:10 +0530

    upgraded files

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class loadingScreen extends StatefulWidget {
@@ -7,29 +6,31 @@ class loadingScreen extends StatefulWidget {
   @override
   _loadingScreenState createState() => _loadingScreenState();
 }
-bool replace = false;
-class _loadingScreenState extends State<loadingScreen> {
-  void replaceRoute()async{
-    await Future.delayed(Duration(milliseconds: 1000));
-    Navigator.pushReplacementNamed(context, '/home');
 
+bool replace = false;
+
+class _loadingScreenState extends State<loadingScreen> {
+  void replaceRoute() async {
+    Future.delayed(const Duration(milliseconds: 1000),
+        () => Navigator.pushReplacementNamed(context, '/home'));
   }
+
   @override
   void initState() {
     super.initState();
     replaceRoute();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
         child: AnimatedContainer(
-          duration: Duration(milliseconds: 1),
+            duration: const Duration(milliseconds: 1),
             height: 200,
             width: 200,
-            child: Image.asset('assets/spotify-logo-png-7053.png')
-        ),
+            child: Image.asset('assets/spotify-logo-png-7053.png')),
       ),
     );
   }
